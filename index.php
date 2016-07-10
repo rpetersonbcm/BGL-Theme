324e28cf1ed331ea0dd391fcc93835a915f4d1fe
Richard Peterson <webbuyrich@gmail.com>
2016-07-09 20:09:43 -0500

    3 icon section set to show category title and information

--- index.php
+++ index.php
@@ -11,6 +11,12 @@
         </ol>
 
         <!-- Wrapper for slides -->
+        <?php 
+        	query_posts( 'post_per_page=1' );
+        		while(have_posts()) : the_post(); ?>
+        <?php endwhile; wp_reset_query(); ?>
+
+
         <div class="carousel-inner">
             <div class="item active">
                 <div class="fill" style="background-image:url('http://placehold.it/1900x1080&text=Slide One');"></div>
@@ -44,48 +50,39 @@
     <!-- Page Content -->
     <div class="container">
 
-        <!-- Marketing Icons Section -->
-        <div class="row">
-            <div class="col-lg-12">
-                <h1 class="page-header">
-                    Welcome to Modern Business
-                </h1>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-check"></i> Bootstrap v3.2.0</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-gift"></i> Free &amp; Open Source</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-4">
-                <div class="panel panel-default">
-                    <div class="panel-heading">
-                        <h4><i class="fa fa-fw fa-compass"></i> Easy to Use</h4>
-                    </div>
-                    <div class="panel-body">
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque, optio corporis quae nulla aspernatur in alias at numquam rerum ea excepturi expedita tenetur assumenda voluptatibus eveniet incidunt dicta nostrum quod?</p>
-                        <a href="#" class="btn btn-default">Learn More</a>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <!-- /.row -->
+       <!-- Big 3 Icon Section -->
+       <div class="row">
+			<div class="col-lg-12">
+			    <h1 class="page-header">
+			        Big 3 Icons
+			    </h1>
+			</div>
+			<?php 
+
+				$cat_args=array(
+			  	'orderby' => 'name',
+			  	'order' => 'ASC',
+			  	'hide_empty' => 0,
+			  	'exclude' => 1		  	
+			   	);
+			
+				$categories = get_categories($cat_args);
+				  foreach ($categories as $cat) {
+
+				        echo '<div class="col-md-4">';
+				        echo '<div class="panel panel-default">';
+				        echo '<div class="panel-heading">';
+				        echo '<h4><i class="fa fa-fw fa-check"></i>'.$cat->name.'</h4>';
+				        echo '</div>';
+				        echo '<div class="panel-body">';
+				        echo '<p>'.$cat->description.'</p>';
+				        echo '</div>';				        
+				        echo '</div>';
+				        echo '</div>';
+				    }
+			?>             
+       </div>
+       <!-- /.row -->
 
         <!-- Portfolio Section -->
         <div class="row">
